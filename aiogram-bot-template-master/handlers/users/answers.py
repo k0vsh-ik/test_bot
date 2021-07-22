c335aefa142f980dcc04cb692beb2d5f6d407996
GOSHU666 <maxtodorov11@gmail.com>
2021-07-22 19:14:39 +0300

    Revert "commit"
    This reverts commit e3b36afdb3a2fcff25c9e966a61e2bbb82510976.
    
    # Conflicts:
    #	aiogram-bot-template-master/handlers/users/answers.py
    #	aiogram-bot-template-master/handlers/users/game.py

--- aiogram-bot-template-master/handlers/users/answers.py
+++ aiogram-bot-template-master/handlers/users/answers.py
@@ -5,11 +5,15 @@ from loader import dp, bot, country_capitals, country, capitals
 from states import Questions
 
 
+ans = []
 @dp.message_handler(state=Questions.Q1)
 async def ans1(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[0]] = answer
+    await state.update_data(
+        {
+            country[0]:answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[1]}")
     await Questions.next()
 
@@ -17,8 +21,11 @@ async def ans1(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q2)
 async def ans2(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[1]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[2]}")
     await Questions.next()
 
@@ -26,8 +33,11 @@ async def ans2(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q3)
 async def ans3(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[2]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[3]}")
     await Questions.next()
 
@@ -35,8 +45,11 @@ async def ans3(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q4)
 async def ans4(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[3]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[4]}")
     await Questions.next()
 
@@ -44,8 +57,11 @@ async def ans4(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q5)
 async def ans5(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[4]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[5]}")
     await Questions.next()
 
@@ -53,8 +69,11 @@ async def ans5(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q6)
 async def ans6(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[5]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[6]}")
     await Questions.next()
 
@@ -62,8 +81,11 @@ async def ans6(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q7)
 async def ans7(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[6]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[7]}")
     await Questions.next()
 
@@ -71,8 +93,11 @@ async def ans7(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q8)
 async def ans8(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[7]] =answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[8]}")
     await Questions.next()
 
@@ -80,18 +105,27 @@ async def ans8(msg: types.Message, state: FSMContext):
 @dp.message_handler(state=Questions.Q9)
 async def ans9(msg: types.Message, state: FSMContext):
     answer = msg.text
-    async with state.proxy() as data:
-        data[country[8]] = answer
+    await state.update_data(
+        {
+            country[0]: answer
+        }
+    )
     await msg.answer(f"Назовите столицу -> {country[9]}")
     await Questions.next()
 
 
 @dp.message_handler(state=Questions.Q10)
-async def ans10(msg: types.Message, state: FSMContext):
+async def ans10(msg: types.Message, state: FSMContext, p=0):
     answer = msg.text
     async with state.proxy() as data:
         data[country[9]] = answer
-
-    await msg.answer("Ответы")
-    await msg.answer(data)
+    data = await state.get_data()
+    for keys in country_capitals:
+        if country_capitals[keys] == data[keys]:
+            ans[p] = 'Верно'
+        else:
+            ans[p] = 'Неверно'
+        p += 1
+    for i in range(10):
+        await msg.answer(f"Ответ на вопрос {i + 1} -> {ans[i]}")
     await state.reset_state()

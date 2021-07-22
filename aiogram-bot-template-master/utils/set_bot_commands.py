7120bff8a8efa27264b28171a5822fa07394e0ab
EVfrat <Vladislav.Koshevoy2@gmail.com>
2021-07-22 19:22:33 +0300

    set_command

--- aiogram-bot-template-master/utils/set_bot_commands.py
+++ aiogram-bot-template-master/utils/set_bot_commands.py
@@ -5,6 +5,7 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
+            types.BotCommand("game", "Начать игру"),
             types.BotCommand("help", "Вывести справку"),
         ]
     )
